5c27e9a48d1e907bfe6f6c8bc4d73aaf6148f5a3
Aadarsh Unni Wilson <waadarsh@outlook.com>
2022-10-30 21:18:30 +0530

    Second Commit

--- controller/operator.js
+++ controller/operator.js
@@ -1,7 +1,7 @@
 var pgp = require("pg-promise")();
 const { publicDecrypt } = require("crypto");
+const path = require("path");
 var queryString = require("querystring");
-// const db = pgp('postgresql://postgres:air2020@localhost:5432/rnaipl');
 const db = pgp('postgresql://postgres:nissan@localhost:5432/rnaipl');
 var chklstId = null;
 var chklstSeqNbr = null;
@@ -105,18 +105,20 @@ exports.postExecuteInspection = function(req, res) {
     var postJudgementValue = req.body.posthiddenJudgementButtonValue;
     console.log("Checklist ID- " + postChklstId + ", Checklist Seq Number- " + postChklstSeqNbr + ", Checklist Detail Id- " + postChklstDtlId + ", Input Field 1: " + postInputField1Value + ", Input Field 2: " + postInputField2Value + ", Comments: " + postCommentValue + ", Judgement- " + postJudgementValue + ", Task Dtl Id- " + postTaskDtlId);
     
+    /*
     if(postInputField1Value != "default") {
-        updateTaskRecords("inputField1", postInputField1Value);
+        updateTaskRecords("inputField1", postInputField1Value, postTaskDtlId);
     }
     if(postInputField2Value != "default") {
-        updateTaskRecords("inputField2", postInputField2Value);
+        updateTaskRecords("inputField2", postInputField2Value, postTaskDtlId);
     }
     if(postCommentValue != "default") {
-        updateTaskRecords("comment", postCommentValue);
+        updateTaskRecords("comment", postCommentValue, postTaskDtlId);
     }
     if(postJudgementValue != "default") {
-        updateTaskRecords("judgement", postJudgementValue);
+        updateTaskRecords("judgement", postJudgementValue, postTaskDtlId);
     }
+    
 
     db.none(`UPDATE public.task_dtl SET status_code = 90, updated_dttm = CURRENT_TIMESTAMP WHERE task_dtl_id = $1;`,[taskDtlId])
     .then(() => {
@@ -129,11 +131,13 @@ exports.postExecuteInspection = function(req, res) {
     .then((data) => {
         if(parseInt(data.max) < (parseInt(postChklstSeqNbr)+1)) {
             console.log("data.max-" + data.max);
-            console.log("postChklstSeqNbr-" + postChklstSeqNbr+1);
+            console.log("postChklstSeqNbr-" + (parseInt(postChklstSeqNbr)+1));
             db.one(`SELECT COUNT(*) FROM public.task_dtl WHERE task_id IN 
             (SELECT task_id from public.task_dtl WHERE task_dtl_id = $1) AND status_code < 90;`,[postTaskDtlId])
             .then((data) => {
+                console.log("data" + data.count);
                 if(data.count === 0) {
+                    console.log("Before update task_hdr, data.count = " + data.count);
                     db.none(`UPDATE public.TASK_HDR set status_code = 90 WHERE task_id IN
                     (SELECT task_id FROM public.task_dtl WHERE task_dtl_id = $1);`,[postTaskDtlId])
                     .then(() => {
@@ -158,18 +162,114 @@ exports.postExecuteInspection = function(req, res) {
     }).catch(error => {
         console.log(error);
     });
+    */
+
+    db.tx(t => {
+        return t.batch(buildUpdateQuery(t, postTaskDtlId, postInputField1Value, postInputField2Value, postCommentValue, postJudgementValue));
+    }).then(() => {
+        db.one(`SELECT MAX(chklst_seq_nbr) FROM public.chklst_dtl WHERE chklst_id = $1;`, [postChklstId])
+        .then((data) => {
+            if(parseInt(data.max) < (parseInt(postChklstSeqNbr) + 1)) {
+                console.log("max(chklstSeqNbr)="+data.max);
+                console.log("chklstSeqNbr="+parseInt(postChklstSeqNbr)+1);
+                db.one(`SELECT COUNT(*) FROM public.task_dtl WHERE task_id IN 
+                (SELECT task_id from public.task_dtl WHERE task_dtl_id = $1) AND status_code < 90;`,[postTaskDtlId])
+                .then((data) => {
+                    console.log("Count(task_dtl)=" + data.count);
+                    if(parseInt(data.count) === 0) {
+                        console.log("Before updating task_hdr");
+                        db.none(`UPDATE public.TASK_HDR set status_code = 90 WHERE task_id IN
+                        (SELECT task_id FROM public.task_dtl WHERE task_dtl_id = $1);`,[postTaskDtlId])
+                        .then(() => {
+                            console.log("Before redirecting");
+                            res.sendFile(path.join(__dirname,"../views/inspectionComplete.html"));
+                        }).catch(error => {
+                            console.log(error);
+                        })
+                    }
+                }).catch(error => {
+                    console.log(error);
+                })
+            }
+            else {
+                chklstId = parseInt(postChklstId);
+                chklstSeqNbr = parseInt(postChklstSeqNbr) + 1;
+                const query = queryString.stringify({
+                    "chklstId":chklstId,
+                    "chklstSeqNbr":chklstSeqNbr
+                });
+                res.redirect("/operator/executeInspection/?" + query);
+            }
+        }).catch(error => {
+            console.log(error);
+        })
+    }).catch(error => {
+        console.log(error);
+    })
 }
 
-function updateTaskRecords(componentName, componentValue) {
+function buildUpdateQuery(t, postTaskDtlId, postInputField1Value, postInputField2Value, postCommentValue, postJudgementValue) {
+    var queryArray = [];
+    if(postInputField1Value != "default") {
+        var inputField1Query = t.none(`UPDATE public.task_component_property SET property_value = $1 WHERE property_type = 'input' AND property_name = 'value' 
+        AND task_component_property_id = 
+        (SELECT tcp.task_component_property_id FROM public.task_component_property tcp
+        INNER JOIN public.task_composite_component_mapping tccm ON tcp.task_component_id = tccm.task_child_component_id AND tcp.property_type='input' 
+        AND tcp.property_name = 'value' AND tccm.task_component_id = 
+        (SELECT tc.task_component_id FROM public.task_component tc
+        INNER JOIN public.chklst_component cc ON cc.chklst_component_id = tc.chklst_component_id AND tc.task_dtl_id = $2
+        INNER JOIN public.component c ON c.component_id = cc.base_component_id AND c.component_name = $3));`,[postInputField1Value, postTaskDtlId, "inputField1"]);
+        queryArray.push(inputField1Query);
+    }
+    if(postInputField2Value != "default") {
+        var inputField2Query = t.none(`UPDATE public.task_component_property SET property_value = $1 WHERE property_type = 'input' AND property_name = 'value' 
+        AND task_component_property_id = 
+        (SELECT tcp.task_component_property_id FROM public.task_component_property tcp
+        INNER JOIN public.task_composite_component_mapping tccm ON tcp.task_component_id = tccm.task_child_component_id AND tcp.property_type='input' 
+        AND tcp.property_name = 'value' AND tccm.task_component_id = 
+        (SELECT tc.task_component_id FROM public.task_component tc
+        INNER JOIN public.chklst_component cc ON cc.chklst_component_id = tc.chklst_component_id AND tc.task_dtl_id = $2
+        INNER JOIN public.component c ON c.component_id = cc.base_component_id AND c.component_name = $3));`,[postInputField2Value, postTaskDtlId, "inputField2"]);
+        queryArray.push(inputField2Query);
+    }
+    if(postCommentValue != "default") {
+        var commentQuery = t.none(`UPDATE public.task_component_property SET property_value = $1 WHERE property_type = 'input' AND property_name = 'value' 
+        AND task_component_property_id = 
+        (SELECT tcp.task_component_property_id FROM public.task_component_property tcp
+        INNER JOIN public.task_composite_component_mapping tccm ON tcp.task_component_id = tccm.task_child_component_id AND tcp.property_type='input' 
+        AND tcp.property_name = 'value' AND tccm.task_component_id = 
+        (SELECT tc.task_component_id FROM public.task_component tc
+        INNER JOIN public.chklst_component cc ON cc.chklst_component_id = tc.chklst_component_id AND tc.task_dtl_id = $2
+        INNER JOIN public.component c ON c.component_id = cc.base_component_id AND c.component_name = $3));`,[postCommentValue, postTaskDtlId, "comment"]);
+        queryArray.push(commentQuery);
+    }
+    if(postJudgementValue != "default") {
+        var judgementQuery =  t.none(`UPDATE public.task_component_property SET property_value = $1 WHERE property_type = 'input' AND property_name = 'value' 
+        AND task_component_property_id = 
+        (SELECT tcp.task_component_property_id FROM public.task_component_property tcp
+        INNER JOIN public.task_composite_component_mapping tccm ON tcp.task_component_id = tccm.task_child_component_id AND tcp.property_type='input' 
+        AND tcp.property_name = 'value' AND tccm.task_component_id = 
+        (SELECT tc.task_component_id FROM public.task_component tc
+        INNER JOIN public.chklst_component cc ON cc.chklst_component_id = tc.chklst_component_id AND tc.task_dtl_id = $2
+        INNER JOIN public.component c ON c.component_id = cc.base_component_id AND c.component_name = $3));`,[postJudgementValue, postTaskDtlId, "judgement"]);
+        queryArray.push(judgementQuery);
+    }
+    var taskDtlQuery = t.none(`UPDATE public.task_dtl SET status_code = 90, updated_dttm = CURRENT_TIMESTAMP WHERE task_dtl_id = $1;`,[postTaskDtlId]);
+    queryArray.push(taskDtlQuery);
 
-    db.none(`UPDATE public.task_component_property SET property_value = $1 WHERE task_component_id =
-    (SELECT tccm.task_child_component_id FROM public.task_composite_component_mapping tccm 
-     INNER JOIN public.task_component tc ON tc.task_component_id = tccm.task_child_component_id AND tc.task_dtl_id = $2
-     INNER JOIN public.chklst_component cc ON cc.chklst_component_id = tc.chklst_component_id
-     INNER JOIN public.component co ON co.component_id = cc.base_component_id
-     INNER JOIN public.composite_component_mapping ccm ON ccm.child_component_id = co.component_id
-     AND ccm.composite_component_id IN (SELECT component_id from public.component where component_name = $3))
-     AND property_type = 'input';`, [componentValue, taskDtlId, componentName])
+    return queryArray;
+}
+
+function updateTaskRecords(componentName, componentValue, postTaskDtlId) {
+
+    db.none(`UPDATE public.task_component_property SET property_value = $1 WHERE property_type = 'input' AND property_name = 'value' 
+    AND task_component_property_id = 
+    (SELECT tcp.task_component_property_id FROM public.task_component_property tcp
+    INNER JOIN public.task_composite_component_mapping tccm ON tcp.task_component_id = tccm.task_child_component_id AND tcp.property_type='input' 
+    AND tcp.property_name = 'value' AND tccm.task_component_id = 
+    (SELECT tc.task_component_id FROM public.task_component tc
+    INNER JOIN public.chklst_component cc ON cc.chklst_component_id = tc.chklst_component_id AND tc.task_dtl_id = $2
+    INNER JOIN public.component c ON c.component_id = cc.base_component_id AND c.component_name = $3));`, [componentValue, postTaskDtlId, componentName])
     .then((data) => {
         console.log("Updated task_component_property: Component Name: " + componentName + " Component Value: " + componentValue);
     }).catch(error => {
@@ -192,6 +292,7 @@ function insertTaskRecords(pgp) {
             ON tc2.chklst_component_id IN 
             (SELECT cccm.chklst_child_component_id FROM public.chklst_composite_component_mapping cccm WHERE tc1.chklst_component_id = cccm.chklst_component_id)
             AND tc1.task_dtl_id = $1
+            AND tc2.task_dtl_id = $1
             AND tc1.composite_component = 'Y';`, [data.task_dtl_id]),
             pgp.none(`INSERT INTO public.task_component_property(task_component_id, property_name, property_value, property_type, created_dttm, updated_dttm)
             SELECT tc.task_component_id, ccp.property_name, ccp.property_value, ccp.property_type, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
